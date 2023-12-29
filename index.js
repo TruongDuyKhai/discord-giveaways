98734ef1bc3a621dc0c3c938cd8f96b35af73794
TruongDuyKhai <102969311+TruongDuyKhai@users.noreply.github.com>
2023-12-29 23:11:58 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 exports.version = require('./package.json').version;
 exports.GiveawaysManager = require('./src/Manager');
 exports.Giveaway = require('./src/Giveaway');
+exports.Events = [
+    'giveawayEnded',
+    'giveawayRerolled',
+    'giveawayDeleted',
+    'giveawayReactionAdded',
+    'giveawayReactionFailed',
+    'endedGiveawayReactionAdded',
+    'giveawayReactionRemoved'
+]
